cdd8c73c8f0d03e1d629af47de373026fb1dd55d
Jonathan Frazier <JohnFrazier@users.noreply.github.com>
2015-12-19 17:11:00 +0000

    refac: prep for json
    pull more into response dict for ease of json transistion.

--- application.py
+++ application.py
@@ -104,24 +104,28 @@ def fb_oauth_request():
 
 @app.route('/logout/', methods=['POST', 'GET'])
 def view_logout():
+    resp = dict(error=False)
     if request.method == 'POST':
         if login_session['state'] == request.form['state']:
             login_session.pop('state', None)
             login_session.pop('user_info', None)
             flash("You have successfully logged out")
-            return redirect("/")
+            resp['html'] = redirect("/")
         else:
             flash("Error: Post contained bad state")
-        return render_template(
-            'logout.html',
-            user_name=login_session['user_info']['name'],
-            state=login_session['state'])
+            resp['error'] = True
+            resp['html'] = redirect("/")
     else:
-        check_active_session(login_session)
-        return render_template(
+        if not check_active_session(login_session):
+            resp['error'] = True
+    if resp['error']:
+        resp['html'] = redirect('/')
+    else:
+        resp['html'] = render_template(
             'logout.html',
             user_name=login_session['user_info']['name'],
             state=login_session['state'])
+    return resp['html']
 
 
 @app.route('/category/')
@@ -135,27 +139,34 @@ def view_category_list():
 
 @app.route('/user/<int:id>/')
 def view_user(id):
-    resp = dict(user=None, items=None)
+    resp = dict(user=None, items=None, error=False)
     try:
         resp['user'] = session.query(User).filter_by(id=id).one()
         resp['items'] = session.query(Item).filter_by(user_id=id).all()
     except db.NoResultFound:
-        resp['items'] = None
-        resp['user'] = None
-    return render_template('view_user.html', items=resp['items'], user=resp['user'])
+        flash("Error: User not found")
+        resp['html'] = redirect('/user/')
+        resp['error'] = True
+    if not resp['error']:
+        resp['html'] = render_template(
+            'view_user.html', items=resp['items'], user=resp['user'])
+    return resp['html']
 
 
 @app.route('/category/<int:id>/')
 def view_category(id):
-    resp = dict(items=[None], cat=None)
+    resp = dict(items=[None], cat=None, error=False)
     try:
         resp['cat'] = session.query(Category).filter_by(id=id).one()
         resp['items'] = session.query(Item).filter_by(category_id=id)
     except db.NoResultFound:
         flash("Category not found")
-        return redirect("/category/")
-    return render_template(
-        'category.html', category=resp['cat'], items=resp['items'])
+        resp['html'] = redirect('/category/')
+        resp['error'] = True
+    if not resp['error']:
+        resp['html'] = render_template(
+            'category.html', category=resp['cat'], items=resp['items'])
+    return resp['html']
 
 
 @app.route('/item/')
@@ -165,54 +176,66 @@ def view_items():
         resp['items'] = session.query(Item).all()
     except db.NoResultFound:
         flash("No items available")
-    return render_template('item.html', items=resp['items'])
+    resp['html'] = render_template('item.html', items=resp['items'])
+    return resp['html']
 
 
 @app.route('/item/<int:id>/', methods=['POST', 'GET'])
 def view_item(id):
-    resp = dict(item=None)
+    resp = dict(item=None, error=False)
     try:
         resp['item'] = session.query(Item).filter_by(
             id=id).one()
     except db.NoResultFound:
         flash("Error: Item not found")
-    if resp['item'] is None:
-        return redirect('/item/')
-    else:
-        if request.method == 'POST':
-            if check_active_session(login_session):
-                if not login_session['user_info']['user_id'] == resp['item'].user_id:
-                    flash("Error: only the item owner can modify this item")
-                else:
-                    if request.form['requestType'] == "edit":
-                        resp['item'].name = request.form['itemName']
-                        resp['item'].category_id = request.form['category_id']
-                        resp['item'].description = request.form['description']
-                        session.add(resp['item'])
-                        session.commit()
-                        flash("%s updated." % resp['item'].name)
-                    elif request.form['requestType'] == "delete":
-                        session.delete(resp['item'])
-                        session.commit()
-                        flash("%s deleted" % resp['item'].name)
-                        return redirect('/item/')
-        return render_template('itemDetail.html', item=resp['item'])
+        resp['error'] = True
+        resp['html'] = redirect('/item/')
+    if request.method == 'POST':
+        if check_active_session(login_session):
+            if not login_session['user_info']['user_id'] == resp['item'].user_id:
+                flash("Error: only the item owner can modify this item")
+                resp['error'] = True
+            else:
+                if request.form['requestType'] == "edit":
+                    resp['item'].name = request.form['itemName']
+                    resp['item'].category_id = request.form['category_id']
+                    resp['item'].description = request.form['description']
+                    session.add(resp['item'])
+                    session.commit()
+                    flash("%s updated." % resp['item'].name)
+                elif request.form['requestType'] == "delete":
+                    session.delete(resp['item'])
+                    session.commit()
+                    flash("%s deleted" % resp['item'].name)
+                    resp['html'] = redirect('/item/')
+    if not resp['error'] and 'html' not in resp.keys():
+        resp['html'] = render_template('itemDetail.html', item=resp['item'])
+    return resp['html']
 
 
 @app.route('/item/<int:id>/edit/')
 def view_item_edit(id):
     resp = dict(item=None)
+    resp['error'] = False
     if check_active_session(login_session):
         try:
             resp['item'] = session.query(Item).filter_by(id=id).one()
         except db.NoResultFound:
             flash("Error: Item not found")
-            return redirect("/item/")
-        if not login_session['user_info']['user_id'] == resp['item'].user_id:
-            flash("Error: only the item owner can edit this item")
-            return redirect("/item/%s/" % id)
-        return render_template('itemEdit.html', item=resp['item'])
-    return redirect('/item/%s/' % id)
+            resp['error'] = True
+        if resp['error']:
+            resp['html'] = redirect("/item/")
+        else:
+            if login_session['user_info']['user_id'] == resp['item'].user_id:
+                resp['html'] = render_template('itemEdit.html', item=resp['item'])
+            else:
+                resp['error'] = True
+                resp['html'] = redirect('/item/%s/' % id)
+                flash("error: only the item owner can edit this item")
+    else:
+        resp['html'] = redirect('/login')
+
+    return resp['html']
 
 
 @app.route('/item/<int:id>/delete/')
@@ -223,27 +246,43 @@ def view_item_delete(id):
             resp['item'] = session.query(Item).filter_by(id=id).one()
         except db.NoResultFound:
             flash("Item not found")
-            return redirect("/item/")
-        if not login_session['user_info']['user_id'] == resp['item'].user_id:
-            flash("Error: only the item owner can delete this item")
-            return redirect("/item/%s/" % id)
-        return render_template('itemDelete.html', item=resp['item'])
-    return redirect("/item/%s/" % id)
+            resp['error'] = True
+        if resp['error']:
+            resp['html'] = redirect("/item/")
+        else:
+            if login_session['user_info']['user_id'] == resp['item'].user_id:
+                resp['html'] = render_template('itemDelete.html', item=resp['item'])
+            else:
+                resp['error'] = True
+                resp['html'] = redirect("/item/%s/" % id)
+                flash("Error: only the item owner can delete this item")
+    else:
+        resp['html'] = redirect('/login')
+
+    return resp['html']
 
 
-@app.route('/item/new/', methods=['POST', 'GET'])
+@app.route('/item/new/', methods=['POST'])
+def view_item_new_post():
+    resp = dict()
+    if check_active_session(login_session):
+        newItem = Item(
+            name=request.form['itemName'],
+            description=request.form['description'],
+            category_id=request.form['category_id'],
+            user_id=login_session['user_info']['user_id'])
+        session.add(newItem)
+        session.commit()
+        flash("%s added to items." % newItem.name)
+        resp['html'] = redirect("/item/")
+    else:
+        resp['html'] = redirect('/login/')
+    return resp['html']
+
+
+@app.route('/item/new/', methods=['GET'])
 def view_item_new():
-    if request.method == 'POST':
-        if check_active_session(login_session):
-            newItem = Item(
-                name=request.form['itemName'],
-                description=request.form['description'],
-                category_id=request.form['category_id'],
-                user_id=login_session['user_info']['user_id'])
-            session.add(newItem)
-            session.commit()
-            flash("%s added to items." % newItem.name)
-            return redirect("/item/")
+    check_active_session(login_session)
     return render_template('itemNew.html')
 
 
@@ -258,6 +297,6 @@ if __name__ == '__main__':
     app.debug = True
     app.secret_key = "something_secret"
     app.testing = False
-    session.add(Category(name="test"))
-    session.commit()
+    # session.add(Category(name="test"))
+    # session.commit()
     app.run(host='0.0.0.0', port=8008)
